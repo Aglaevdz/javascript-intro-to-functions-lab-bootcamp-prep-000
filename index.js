411d04b764582c14937a63ff277b40145691fc8d
Aglae Verduzco <aglae.cv@gmail.com>
2018-09-16 22:45:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function sayHiToGrandma(string) {
  else if (string.toUpperCase() === string) 
  return("YES INDEED!");
  else if (string === 'I love you, Grandma')
+ console.log('I love you, Grandma')
  return("I love you, too.")
   }
   
